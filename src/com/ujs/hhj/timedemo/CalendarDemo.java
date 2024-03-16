e5a6665ad1645a567dcd474e9683a96ca7ddee67
hhj <449216786@qq.com>
2024-03-16 19:40:28 +0800

    CalendarDemo

---
+++ src/com/ujs/hhj/timedemo/CalendarDemo.java
@@ -0,0 +1,17 @@
+package com.ujs.hhj.timedemo;
+
+import java.util.Calendar;
+
+public class CalendarDemo {
+    public static void main(String[] args) {
+//        创建Calendar对象
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar);
+
+//        获取年
+        int year = calendar.get(Calendar.YEAR);
+        System.out.println("年: " + year);
+        calendar.add(Calendar.YEAR, 1);
+        System.out.println("年: " + calendar.get(Calendar.YEAR));
+    }
+}
