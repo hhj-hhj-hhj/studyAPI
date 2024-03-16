cde5b49dd56f3eb7b4d2ac05797294a182a9e36f
hhj <449216786@qq.com>
2024-03-16 18:12:46 +0800

    String正则

--- src/com/ujs/hhj/patterndemo/StringPattern.java
+++ src/com/ujs/hhj/patterndemo/StringPattern.java
@@ -3,5 +3,23 @@ package com.ujs.hhj.patterndemo;
 public class StringPattern {
     public static void main(String[] args) {
 
+//        tihuan();
+//        fenge();
+
+    }
+
+    private static void fenge() {
+        String str = "胡伽琪abcde 黄宏进def 真老师";
+        String[] s = str.split("[ \\w&&[^_]]+");
+        for (String s1 : s) {
+            System.out.println(s1);
+        }
+    }
+
+    private static void tihuan() {
+        //        替换
+        String str = "胡伽琪abcde 黄宏进def 真老师";
+        String s = str.replaceAll("[\\w&&[^_]]+", "vs");
+        System.out.println(s);
     }
 }
