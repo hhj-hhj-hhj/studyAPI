8cf4d17920adb0847eb87f42ad0cb9a4ff1db1f4
hhj <449216786@qq.com>
2024-03-23 17:11:32 +0800

    List的基本方法使用

---
+++ src/com/ujs/hhj/containers/collection/lists/ListDemo.java
@@ -0,0 +1,36 @@
+package com.ujs.hhj.containers.collection.lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        init(list);
+        forEach1(list);
+        forEach2(list);
+        forEach3(list);
+    }
+
+    private static void forEach3(List<String> list) {
+        for(String s : list){
+            System.out.println(s);
+        }
+    }
+
+    private static void forEach2(List<String> list) {
+        list.forEach(System.out::println);
+    }
+
+    private static void init(List<String> list) {
+        list.add("hello");
+        list.add("world");
+        list.add("java");
+    }
+
+    private static void forEach1(List<String> list) {
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
+}
