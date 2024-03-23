5a87df1fdda2354c20142960a4d4c73c59b39abb
hhj <449216786@qq.com>
2024-03-23 17:16:56 +0800

    List的基本方法使用

--- src/com/ujs/hhj/containers/collection/lists/ListDemo.java
+++ src/com/ujs/hhj/containers/collection/lists/ListDemo.java
@@ -2,6 +2,7 @@ package com.ujs.hhj.containers.collection.lists;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ListDemo {
     public static void main(String[] args) {
@@ -10,11 +11,14 @@ public class ListDemo {
         forEach1(list);
         forEach2(list);
         forEach3(list);
+        forEach4(list);
     }
-    private static void init(List<String> list) {
-        list.add("hello");
-        list.add("world");
-        list.add("java");
+
+    private static void forEach4(List<String> list) {
+        ListIterator<String> listIterator = list.listIterator();
+        while(listIterator.hasNext()){
+            System.out.println(listIterator.next());
+        }
     }
 
     private static void forEach3(List<String> list) {
@@ -31,5 +35,11 @@ public class ListDemo {
         for(int i = 0; i < list.size(); i++){
             System.out.println(list.get(i));
         }
+    }
+
+    private static void init(List<String> list) {
+        list.add("hello");
+        list.add("world");
+        list.add("java");
     }
 }
