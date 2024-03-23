08f24bd728449f7cfea0bf43424eeeab80c6c2f5
hhj <449216786@qq.com>
2024-03-23 14:16:30 +0800

    Lambda表达式

---
+++ src/com/ujs/hhj/lambda/LambdaDemo.java
@@ -0,0 +1,32 @@
+package com.ujs.hhj.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LambdaDemo {
+    public static void main(String[] args) {
+        fun1();
+        fun2();
+    }
+
+    private static void fun2() {
+        System.out.println("-------lambda表达式-------");
+        Integer[] arr = {1, 4, 5, 3, 2};
+//        可以省略参数类型
+        Arrays.sort(arr, (o1, o2) -> {
+            return o2 - o1;});
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void fun1() {
+        System.out.println("-------匿名内部类形式-------");
+        Integer[] arr = {1, 4, 5, 3, 2};
+        Arrays.sort(arr, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        System.out.println(Arrays.toString(arr));
+    }
+}
