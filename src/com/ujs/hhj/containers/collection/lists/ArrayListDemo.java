fd2185e9b0ce24f1e179eff45768e35ee92989d2
HHJ <449216786@qq.com>
2024-03-25 19:57:46 +0800

    Lists

---
+++ src/com/ujs/hhj/containers/collection/lists/ArrayListDemo.java
@@ -0,0 +1,9 @@
+package com.ujs.hhj.containers.collection.lists;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+//        底层是数组，查询快，增删慢
+//        扩容方式：默认为原容量的1.5倍，若原容量为0，则扩容为10。若原容量大于0，则扩容为原容量的1.5倍。若1.5倍后的容量小于指定容量，则扩容为指定容量。
+//        扩容时，会将原数组中的元素复制到新数组中。
+    }
+}
---
+++ src/com/ujs/hhj/containers/collection/lists/LinkedListDemo.java
@@ -0,0 +1,8 @@
+package com.ujs.hhj.containers.collection.lists;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+//        底层是双向链表，查询慢，增删快
+//        增删时，不需要移动元素，只需要修改指针即可。
+    }
+}
